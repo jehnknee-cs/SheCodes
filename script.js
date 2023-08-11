e886e91ec6d680ad62b856f19206ef3660373342
jehnknee-cs <jejenniferescobar@gmail.com>
2023-08-11 01:10:29 -0700

    adjusted classes

--- script.js
+++ script.js
@@ -60,13 +60,17 @@ function handleSubmit(event) {
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  let celsiusTemperature = (fahrenheitTemperature - 32) * (5/9);
   let temperatureElement = document.querySelector("#temperature");
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let celsiusTemperature = (fahrenheitTemperature - 32) * (5/9);
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
