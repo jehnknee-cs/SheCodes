8625e55394c26cf7b6b1dafcab96101b2cf993ff
jehnknee-cs <jejenniferescobar@gmail.com>
2023-08-10 22:57:10 -0700

    Changed weather display from celsius to fahrenheit

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function displayIcon(response) {
 
 function searchCity(city) {
   let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
