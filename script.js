2e557e89dffd9e05bb2a7821042701c5722f62df
jehnknee-cs <jejenniferescobar@gmail.com>
2023-08-17 12:13:25 -0700

    Injected HTML into JS file to create weekly forecast.

--- script.js
+++ script.js
@@ -22,6 +22,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img src="http://openweathermap.org/img/wn/01d@2x.png" />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperatures-max">18°</span>
+                <span class="weather-forecast-temperatures-min">12°</span>
+              </div>
+            </div>
+          `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -63,7 +86,7 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
   fahrenheitLink.classList.remove("active");
   celsiusLink.classList.add("active");
-  let celsiusTemperature = (fahrenheitTemperature - 32) * (5/9);
+  let celsiusTemperature = (fahrenheitTemperature - 32) * (5 / 9);
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
@@ -87,3 +110,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Riverside");
+displayForecast();
